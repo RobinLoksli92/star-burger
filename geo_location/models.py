9d1ded352ea282a3f72f8d7e969b315335c371bd
RobinLoksli92 <gleb11tiun@mail.ru>
2022-09-17 00:17:06 +0200

    Changed uniqueness for address in GeoLocation

--- geo_location/models.py
+++ geo_location/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 
 class GeoLocation(models.Model):
-    address = models.CharField('Адрес', max_length=200)
+    address = models.CharField('Адрес', max_length=200, unique=True)
     lat = models.DecimalField('Широта', max_digits=10, decimal_places=5, null=True)
     long = models.DecimalField('Долгота', max_digits=10, decimal_places=5, null=True)
     request_date = models.DateTimeField('Дата запроса к геокодеру', default=timezone.now)
