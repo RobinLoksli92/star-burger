6701595e9bf8006aee34d1ba688448fbb777e32c
RobinLoksli92 <gleb11tiun@mail.ru>
2023-01-12 13:18:05 +0000

    Add server id to allowed_hosts

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -13,9 +13,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 YANDEX_APIKEY = env('YANDEX_APIKEY')
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = env.bool('DEBUG', True)
+DEBUG = env.bool('DEBUG', False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost', '45.131.41.135'])
+<<<<<<< Updated upstream
 
 ROLLBAR = {
     'access_token': env('ROLLBAR_TOKEN'),
@@ -23,6 +24,8 @@ ROLLBAR = {
     'code_version': '1.0',
     'root': BASE_DIR,
 }
+=======
+>>>>>>> Stashed changes
 
 INSTALLED_APPS = [
     'geo_location.apps.GeoLocationConfig',
